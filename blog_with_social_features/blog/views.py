7b403c7d71630de88d9a415c5956b5b1f38b32bc
iamjnayem <iamj.nayem@gmail.com>
2025-03-01 00:27:08 +0600

    make url seo friendly

--- blog_with_social_features/blog/views.py
+++ blog_with_social_features/blog/views.py
@@ -16,7 +16,13 @@ def post_list(request):
 #         raise Http404('Post not found')
 #     return render(request, 'blog/post/detail.html', {'post': post}) 
 
-def post_detail(request,id):
-    post = get_object_or_404(Post, id=id)
+def post_detail(request,year,month,day,post):
+    post = get_object_or_404(
+        Post, status=Post.Status.PUBLISHED,
+        slug=post,publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
+    
     return render(request, 'blog/post/detail.html', {'post': post}) 
 
